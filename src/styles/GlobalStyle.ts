530c445c1872ffcd1d73472a150452d816d143a7
JisuKim <jso_osj@naver.com>
2022-08-07 19:47:41 +0900

    CHORE :wrench: styled-reset 설치 및 globalStyle 작성

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+import { reset } from "styled-reset";
+
+export const GlobalStyle = createGlobalStyle`
+  ${reset}
+  
+  * {
+    box-sizing: border-box;
+  }
+  html {
+    font-size: 62.5%;
+    height: 100%;
+    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  input, button {
+    background-color: transparent;
+    border: none;
+    outline: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
