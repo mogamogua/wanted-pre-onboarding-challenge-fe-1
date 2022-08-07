64660e5a5d4e147b6c46754aec7b36c4e7a8ead9
JisuKim <jso_osj@naver.com>
2022-08-07 21:50:58 +0900

    FEAT :sparkles: colors.ts 추가 및 globalStyle 변경

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -20,8 +20,6 @@ export const GlobalStyle = createGlobalStyle`
 
   input, button {
     background-color: transparent;
-    border: none;
-    outline: none;
   }
 
   button {
---
+++ src/styles/colors.ts
@@ -0,0 +1,14 @@
+const colors = {
+  mainPurple: "#524fa1",
+  subPurple: "#b3b0ee",
+  mainBlack: "#222",
+  textGray1: "#5e5f61",
+  textGray2: "#7d7e80",
+  textGray3: "#999",
+  borderGray: "#C9CACC",
+  lightGray: "#ccc",
+  bgGray: "#f0f1f3",
+  white: "#FFFFFF",
+};
+
+export default colors;
