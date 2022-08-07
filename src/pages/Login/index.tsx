df8aae382a8f3183b5fc89965172eb995f59eaf1
JisuKim <jso_osj@naver.com>
2022-08-07 23:38:29 +0900

    FIX :hammer: Signup 컴포넌트 삭제하고 Login컴포넌트로 통합

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import LoginForm from "./LoginForm";
 import { LoginBox, Message, StyledLink, StyledRoot, Title } from "./style";
 
@@ -7,14 +7,20 @@ function Login() {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
   if (token) navigate("/");
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/auth/login" ? true : false;
 
   return (
     <StyledRoot>
       <LoginBox>
-        <Title>투두리스트 로그인</Title>
-        <LoginForm />
-        <Message>아직 회원이 아니신가요?</Message>
-        <StyledLink to="/auth/sign_up">회원가입 하러가기</StyledLink>
+        <Title>{isLoginPage ? "투두리스트 로그인" : "투두리스트 회원가입"}</Title>
+        {isLoginPage ? <LoginForm /> : null}
+        <Message>{isLoginPage ? "아직 회원이 아니신가요?" : "이미 회원이신가요?"}</Message>
+        {isLoginPage ? (
+          <StyledLink to="/auth/sign_up">회원가입 하러가기</StyledLink>
+        ) : (
+          <StyledLink to="/auth/login">로그인 하러가기</StyledLink>
+        )}
       </LoginBox>
     </StyledRoot>
   );
--- src/pages/Routers.tsx
+++ src/pages/Routers.tsx
@@ -1,14 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Main from "./Main";
-import SignUp from "./SignUp";
 
 function Routers() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/auth/sign_up" element={<SignUp />} />
+        <Route path="/auth/sign_up" element={<Login />} />
         <Route path="/auth/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
