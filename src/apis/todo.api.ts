20a8edeb61b14229d7bf40c61f831f9c854a50d5
JisuKim <jso_osj@naver.com>
2022-08-08 04:51:09 +0900

    FEAT : create todo api 작성

--- src/apis/todo.api.ts
+++ src/apis/todo.api.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { baseAxios } from "./axios"
 
 export const getTodos = async (token: string) => {
@@ -11,6 +12,25 @@ export const getTodos = async (token: string) => {
       return apiRes.data;
     }
   } catch (error) {
-    throw error;
+      throw error;
   }
 };
+
+export const createTodo = async (token: string, title:string, content:string) => {
+  try {
+    const apiRes = await baseAxios.post("/todos", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: {
+        title,
+        content,
+      }
+    });
+    if (apiRes.status === 200) {
+      return apiRes.data;
+    }
+  } catch (error) {
+    throw error;
+  }
+}
